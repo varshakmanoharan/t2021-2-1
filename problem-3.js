293bc687af8f08bad3ecc37dec309345a354369d
unknown <varshakmanoharan>
2022-09-22 15:22:44 +0530

    added

--- problem-3.js
+++ problem-3.js
@@ -3,7 +3,7 @@ const ps =require("prompt-sync");
 const prompt =ps();
     const n = prompt("Enter the limit:");
     let i;
-    for(i=1;i<=n; i+=2)
+    for(i=1;i<=n;i+=2)
     {
         console.log(i);
         
